40eae2049f9c53ed7922f4a051b2227586ee9eea
drew-fugate <drew.fugate@wavelengthusaf.com>
2023-05-04 08:48:47 -0500

    Made a second block under main photo on home

--- tommy/src/App.tsx
+++ tommy/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import headshot from './images/ComebackPromo002 2.png';
+import Box from '@mui/material/Box';
 
 import './App.css';
 
@@ -9,7 +10,17 @@ function App() {
       <header className="App-header">
         
       </header>
+      <Box>
       <img className="headshot" src={headshot} alt="headshot" />
+      </Box>
+
+
+      <div className="clickableBackground">
+      <Box>
+        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae amet laborum eius neque expedita cumque ipsam error esse ex? Tempore, aspernatur atque veritatis earum velit perferendis illum quae nobis illo.
+        </p>
+      </Box>
+      </div>
     </div>
   );
 }
