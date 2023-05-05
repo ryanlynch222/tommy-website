ffb12c938d95275cd7c8f713d77d15f99cc533ca
drew-fugate <drew.fugate@wavelengthusaf.com>
2023-05-05 11:16:25 -0500

    Added some album covers using react maps

--- tommy/src/App.tsx
+++ tommy/src/App.tsx
@@ -1,25 +1,61 @@
 import React from 'react';
 import headshot from './images/ComebackPromo002 2.png';
-import Box from '@mui/material/Box';
 
 import './App.css';
 
+const albums = [
+  {
+    title: 'Ruby Bottles',
+    date: '2023',
+    description: 'description of Ruby Bottles',
+    image: require('./images/bottles.png'),
+  },
+  {
+    title: 'Emeralds and Jade',
+    date: '2021',
+    description: 'description of Emeralds and Jade',
+    image: require('./images/crystals.png')
+  },
+  {
+    title: 'Numb It',
+    date: '2020',
+    description: 'description of Numb It',
+    image: require('./images/whiskey.png')
+  },
+  {
+    title: 'The Marked',
+    date: '2019',
+    description: 'description of The Marked',
+    image: require('./images/hand.png')
+  },
+  {
+    title: 'An Introduction',
+    date: '2019',
+    description: 'description of An Introduction',
+    image: require('./images/kingfisher.png')
+  }
+]
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         
       </header>
-      <Box>
       <img className="headshot" src={headshot} alt="headshot" />
-      </Box>
-
 
       <div className="clickableBackground">
-      <Box>
-        <p >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae amet laborum eius neque expedita cumque ipsam error esse ex? Tempore, aspernatur atque veritatis earum velit perferendis illum quae nobis illo.
-        </p>
-      </Box>
+          <h4 className="disco">Discography</h4>
+      </div>
+      <div className="album-grid">
+        {albums.map((album, index) => (<div key={index} className="album"><img src={album.image} 
+        alt={album.title}/>
+        <h4>{album.title}</h4>
+        <p>{album.date}</p>
+        <p>{album.description}</p>
+        </div>
+))}
       </div>
     </div>
   );
