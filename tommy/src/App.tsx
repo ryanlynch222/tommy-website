7e0af6de664434d75b27069147c2ae8322ed3f9c
drew-fugate <drew.fugate@wavelengthusaf.com>
2023-05-05 11:56:26 -0500

    added portrait and text

--- tommy/src/App.tsx
+++ tommy/src/App.tsx
@@ -1,61 +1,69 @@
-import React from 'react';
-import headshot from './images/ComebackPromo002 2.png';
+import React from "react";
+import headshot from "./images/headshot.png";
+import portrait from "./images/portrait.png";
 
-import './App.css';
+import "./App.css";
 
 const albums = [
   {
-    title: 'Ruby Bottles',
-    date: '2023',
-    description: 'description of Ruby Bottles',
-    image: require('./images/bottles.png'),
+    title: "Ruby Bottles",
+    date: "2023",
+    description: "description of Ruby Bottles",
+    image: require("./images/bottles.png"),
   },
   {
-    title: 'Emeralds and Jade',
-    date: '2021',
-    description: 'description of Emeralds and Jade',
-    image: require('./images/crystals.png')
+    title: "Emeralds and Jade",
+    date: "2021",
+    description: "description of Emeralds and Jade",
+    image: require("./images/crystals.png"),
   },
   {
-    title: 'Numb It',
-    date: '2020',
-    description: 'description of Numb It',
-    image: require('./images/whiskey.png')
+    title: "Numb It",
+    date: "2020",
+    description: "description of Numb It",
+    image: require("./images/whiskey.png"),
   },
   {
-    title: 'The Marked',
-    date: '2019',
-    description: 'description of The Marked',
-    image: require('./images/hand.png')
+    title: "The Marked",
+    date: "2019",
+    description: "description of The Marked",
+    image: require("./images/hand.png"),
   },
   {
-    title: 'An Introduction',
-    date: '2019',
-    description: 'description of An Introduction',
-    image: require('./images/kingfisher.png')
-  }
-]
+    title: "An Introduction",
+    date: "2019",
+    description: "description of An Introduction",
+    image: require("./images/kingfisher.png"),
+  },
+];
 
 function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <header className="App-header"></header>
       <img className="headshot" src={headshot} alt="headshot" />
-
       <div className="clickableBackground">
-          <h4 className="disco">Discography</h4>
+        <h4 className="disco">Discography</h4>
       </div>
       <div className="album-grid">
-        {albums.map((album, index) => (<div key={index} className="album"><img src={album.image} 
-        alt={album.title}/>
-        <h4>{album.title}</h4>
-        <p>{album.date}</p>
-        <p>{album.description}</p>
-        </div>
-))}
+        {albums.map((album, index) => (
+          <div key={index} className="album">
+            <img src={album.image} alt={album.title} />
+            <h4>{album.title}</h4>
+            <p>{album.date}</p>
+            <p>{album.description}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="aboutMe">
+        <p className="aboutText">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          eaque molestiae consequuntur eveniet minus eum dolor fugit delectus,
+          illo ducimus blanditiis est in modi exercitationem corrupti
+          recusandae, beatae accusamus qui.
+        </p>
+        <img className="portrait" src={portrait} alt="portrait" />
       </div>
     </div>
   );
